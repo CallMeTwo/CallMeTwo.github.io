9f3fae77c0a7643747832fec201d98088940e82f
CallMeTwo <66190973+CallMeTwo@users.noreply.github.com>
2022-10-23 16:29:02 +0700

    Add files via upload

---
+++ project/Config.py
@@ -0,0 +1,3 @@
+secret = "7db5cd0d73eb2aec77a0bd5fcc793661"
+token = "pHwpPlUXu1dN54cUgRD9n8W/fuayb/LDtxkNUzEXRUHINfmyn83+BdVxs1DPRrFxL3kZSR4lbq1L/ov0YIKTZEvn/M5FG+VAUAhLE8HNs/9nJomwkqm0fvZ+X9HWczeV2uoGKa0dC6uRqPd9othzmQdB04t89/1O/w1cDnyilFU="
+basic = "@850hrsev"
