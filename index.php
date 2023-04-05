f2d21f14fba40dfcb6b3332e6826435f7f5b61e9
CallMeTwo <chanawee4@hotmail.com>
2023-04-05 21:25:32 +0700

    del

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php echo file_get_contents("html/header.html"); ?>
-<?php echo file_get_contents("html/body.html"); ?>
-<?php echo file_get_contents("html/footer.html"); ?>
-<p>
-    Copyright &copy; Two <?php echo date("Y"); ?>
-</p>
